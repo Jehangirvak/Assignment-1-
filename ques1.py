35940b7ebfdffdc30dc4e782a14ad33f0fd159de
Jehangir Vakil <32462915+Jehangirvak@users.noreply.github.com>
2019-11-03 17:42:34 +0500

    Delete ques1.py

--- ques1.py
+++
@@ -1,1 +0,0 @@
-print ("Twinkle, twinkle, little star,\n\t How I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\n Twinkle, twinkle, little star,\n\t How I wonder what you are")
