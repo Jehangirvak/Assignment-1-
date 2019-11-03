75f727f1f19a1edca8d71966dd911bc52faf7531
Jehangir Vakil <jvakil77@gmail.com>
2019-11-03 17:07:29 +0500

    Assignment 1

---
+++ ques2.py
@@ -0,0 +1,5 @@
+import sys
+print("Python curretnt version")
+print (sys.version)
+print("Version information")
+print (sys.version_info)
---
+++ ques4.py
@@ -0,0 +1,3 @@
+from math import pi
+radius=int(input("Enter the raius of circle: "))
+print ("The area is: ",radius*((pi)**2))
